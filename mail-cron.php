a72ef0e6378de33d2096e3f7aad3b2a53923d401
Anubir <eddythemeddy@gmail.com>
2020-04-02 01:32:58 +0000

    latest

--- mail-cron.php
+++ mail-cron.php
@@ -14,13 +14,15 @@ $config = [
 new Database($config);
 
 $eq = $eqDb;
+$hash = $_GET['hash'];
 $eqDb->where('hash', $hash);
 $check = $eqDb->get('mail');
+//var_dump($check);
 if(count($check)) { 
     $eqDb->where('hash', $hash);
     $update = $eqDb->update('mail',[
-        'read' => 1
+       'read' => 1,
+       'server' => $_SERVER['HTTP_HOST']
     ]);
-
-    var_dump($update);
+//var_dump(json_stringify($_SERVER));	
 }
